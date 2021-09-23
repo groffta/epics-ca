841a2e03035e9c0a45104d2c8b85d16bbc47de63
Todd <groffta@ornl.gov>
2021-09-23 10:38:49 -0400

    added version and port constants. init_repeater placehodler

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
+// Imports
+
+// Re-exports
 pub mod protocol;
+pub mod repeater;
+pub use repeater::init_repeater;
+
+// EPICS Channel Access Protocol Version
+pub const MAJOR_PROTOCOL_VERSION: u16 = 4;
+pub const MINOR_PROTOCOL_VERSION: u16 = 11;
+
+// Port Number Constants
+const CA_PORT_BASE: u16 = 5056;
+pub const CA_SERVER_PORT: u16 = CA_PORT_BASE + MAJOR_PROTOCOL_VERSION * 2;
+pub const CA_REPEATER_PORT: u16 = CA_PORT_BASE + MAJOR_PROTOCOL_VERSION *2 + 1;
+
---
+++ src/repeater.rs
@@ -0,0 +1,5 @@
+
+/// Initializes a new repeater or connects to an existing repeater
+pub fn init_repeater() -> Result<(), std::io::Error> {
+    todo!();
+}
