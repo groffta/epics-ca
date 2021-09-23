345875717b6f23aac43e6aed416f6c87e587a85c
Todd <groffta@ornl.gov>
2021-09-23 12:01:08 -0400

    Implemented repeater presence check

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-// Imports
-
 // Re-exports
 pub mod protocol;
 pub mod repeater;
-pub use repeater::init_repeater;
+
+// Imports
+
 
 // EPICS Channel Access Protocol Version
 pub const MAJOR_PROTOCOL_VERSION: u16 = 4;
@@ -13,4 +13,8 @@ pub const MINOR_PROTOCOL_VERSION: u16 = 11;
 const CA_PORT_BASE: u16 = 5056;
 pub const CA_SERVER_PORT: u16 = CA_PORT_BASE + MAJOR_PROTOCOL_VERSION * 2;
 pub const CA_REPEATER_PORT: u16 = CA_PORT_BASE + MAJOR_PROTOCOL_VERSION *2 + 1;
+
+pub fn init() {
+    repeater::init();
+}
 
--- src/repeater.rs
+++ src/repeater.rs
@@ -1,5 +1,35 @@
+use std::net::{
+    UdpSocket,
+    SocketAddr,
+    IpAddr,
+    Ipv4Addr,
+};
+
+use log::{info, error};
+
 
 /// Initializes a new repeater or connects to an existing repeater
-pub fn init_repeater() -> Result<(), std::io::Error> {
-    todo!();
+pub fn init() {
+    // Check for existing repeater by attempting to bind to repeater port
+    match UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0,0,0,0,)), crate::CA_REPEATER_PORT)) {
+        Ok(socket) => {
+            // No repeater bound. Close socket and spawn a new repeater.
+            drop(socket);
+            spawn_repeater();
+        }
+        Err(_e) => {
+            // Repeater is already bound. Perform repeater registration.
+            register();
+        }
+    }
+}
+
+fn spawn_repeater() {
+    info!("Spawning new repeater on 0.0.0.0:{}", crate::CA_REPEATER_PORT);
+    todo!()
+}
+
+fn register() {
+    info!("Registering with existing repeater at 127.0.0.1:{}", crate::CA_REPEATER_PORT);
+    todo!()
 }
